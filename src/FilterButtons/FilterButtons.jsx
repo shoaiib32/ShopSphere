208f85bcf046bfd0a63959373deac8c15fbc6665
Shoaib Siddique <siddiqueshoaib38@gmail.com>
2024-10-31 19:15:16 +0530

    basic changes

--- src/FilterButtons/FilterButtons.jsx
+++ src/FilterButtons/FilterButtons.jsx
@@ -6,7 +6,7 @@ const FilterButtons = ({ handleCancelClick, activeeCategory, onFilterChange }) =
 
   const handleButtonClick = (filter) => {
     setButton(filter);
-    onFilterChange(filter); // Pass the filter to the parent
+    onFilterChange(filter); sidebarToggleSlice Pass the filter to the parent
   };
 
   return (
